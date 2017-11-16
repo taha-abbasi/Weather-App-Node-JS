82297fa5ec951b5dc5f682c29d2241177db68610
Taha Abbasi <tahaabbasi@me.com>
2017-11-16 01:10:47 -0500

    location based weather implemented

--- weather.js
+++ weather.js
@@ -1,7 +1,15 @@
 var request = require('request');
-var url = 'http://api.openweathermap.org/data/2.5/weather?appid=7e3f1dd378bef34937935f0128e0a29c&q=Philadelphia&units=imperial';
 
-module.exports = function (callback) {
+module.exports = function (location, callback) {
+
+    var encodedLocation = encodeURIComponent(location);
+    var url = 'http://api.openweathermap.org/data/2.5/weather?appid=7e3f1dd378bef34937935f0128e0a29c&q=' + encodedLocation + '&type=like&units=imperial';
+
+
+    if (!location) {
+        return callback('No location provided');
+    }
+
     request({
         url: url,
         json: true
@@ -9,6 +17,9 @@ module.exports = function (callback) {
         if (error) {
             callback('Unable to fetch weather');
         } else {
+            if (body.cod === '404') {
+                return callback(body.message);
+            }
             // console.log(JSON.stringify(body, null, 4));
             callback('It\'s ' + body.main.temp + ' in ' + body.name + '!');
         }
