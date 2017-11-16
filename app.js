44f9f2cc357da3ebe70619874ee69327852919aa
Taha Abbasi <tahaabbasi@me.com>
2017-11-16 00:00:47 -0500

    location detection implemented

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var weather = require('./weather.js');
+var location = require('./location.js');
 
 weather(function (currentWeather) {
     console.log(currentWeather);
 });
 
+location(function (location) {
+    if (!location) {
+        console.log('Unable to fetch location.');
+        return;
+    }
+    console.log('city: ' + location.city);
+    console.log('log/lat: ' + location.loc);
+});
