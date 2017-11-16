403aa546b8708fb49485102d558258ff4d0f5fc6
Taha Abbasi <tahaabbasi@me.com>
2017-11-16 11:00:01 -0500

    Promises implemented

--- weather.js
+++ weather.js
@@ -1,27 +1,31 @@
 var request = require('request');
 
-module.exports = function (location, callback) {
+module.exports = function (location) {
 
-    var encodedLocation = encodeURIComponent(location);
-    var url = 'http://api.openweathermap.org/data/2.5/weather?appid=7e3f1dd378bef34937935f0128e0a29c&q=' + encodedLocation + '&type=like&units=imperial';
+    return new Promise(function (resolve, reject) {
+        var encodedLocation = encodeURIComponent(location);
+        var url = 'http://api.openweathermap.org/data/2.5/weather?appid=7e3f1dd378bef34937935f0128e0a29c&q=' + encodedLocation + '&type=like&units=imperial';
 
 
-    if (!location) {
-        return callback('No location provided');
-    }
-
-    request({
-        url: url,
-        json: true
-    }, function (error, response, body) {
-        if (error) {
-            callback('Unable to fetch weather');
-        } else {
-            if (body.cod === '404') {
-                return callback(body.message);
-            }
-            // console.log(JSON.stringify(body, null, 4));
-            callback('It\'s ' + body.main.temp + ' in ' + body.name + '!');
+        if (!location) {
+            reject('No location provided');
         }
+
+        request({
+            url: url,
+            json: true
+        }, function (error, response, body) {
+            if (error) {
+                reject('Unable to fetch weather');
+            } else {
+                if (body.cod === '404') {
+                    reject(body.message);
+                } else {
+                    resolve('It\'s ' + body.main.temp + ' in ' + body.name + '!');
+                }
+                // console.log(JSON.stringify(body, null, 4));
+
+            }
+        });
     });
 };
